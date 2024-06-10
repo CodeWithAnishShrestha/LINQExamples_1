d5142190c2e1dbf0cbf4ae4cd55b5d5097470c78
CodeWithAnishShrestha <growwithanish@gmail.com>
2024-06-10 13:53:23 +0545

    Group join and outer join

--- LINQExamples_1/Program.cs
+++ LINQExamples_1/Program.cs
@@ -21,28 +21,141 @@
             List<Employee> employeeList = Data.GetEmployees();
             List<Department> departmentList = Data.GetDepartments();
 
-            var results = from emp in employeeList
-                          where emp.AnnualSalary >= 50000
+            // Select and where operators = Query Syntax
+            //var results = from emp in employeeList
+            //              where emp.AnnualSalary >= 50000
+            //              select new
+            //              {
+            //                  FullName = emp.FirstName + " " + emp.LastName,
+            //                  AnnualSalary = emp.AnnualSalary
+            //              };
+
+            //foreach (var item in results)
+            //{
+            //    Console.WriteLine($"{item.FullName,-20}{item.AnnualSalary,10}");
+            //}
+
+            //query is not used here due to deferred execution
+            //var results = from emp in employeeList.GetHighSalariedEmployees()
+            //              select new
+            //              {
+            //                  FullName = emp.FirstName + " " + emp.LastName,
+            //                  AnnualSalary = emp.AnnualSalary
+            //              };
+            //employeeList.Add(new Employee
+            //{
+            //    Id = 5,
+            //    FirstName = "Sandesh",
+            //    LastName = "Rijal",
+            //    AnnualSalary = 100000.56m,
+            //    IsManager = true,
+            //    DepartmentId = 2
+            //});
+
+            ////Query is executed here when used in interation
+            //foreach (var item in results)
+            //{
+            //    Console.WriteLine($"{item.FullName,-20}{item.AnnualSalary,10}");
+            //}
+
+            //Immediate Execution Example
+            //var results = (from emp in employeeList.GetHighSalariedEmployees()
+            //               select new
+            //               {
+            //                   FullName = emp.FirstName + " " + emp.LastName,
+            //                   AnnualSalary = emp.AnnualSalary
+            //               }).ToList();
+            //employeeList.Add(new Employee
+            //{
+            //    Id = 5,
+            //    FirstName = "Sandesh",
+            //    LastName = "Rijal",
+            //    AnnualSalary = 100000.56m,
+            //    IsManager = true,
+            //    DepartmentId = 2
+            //});
+            //foreach (var item in results)
+            //{
+            //    Console.WriteLine($"{item.FullName,-20}{item.AnnualSalary,10}");
+            //}
+
+            //Join Operation Example - Method Syntax
+            //var results = departmentList.Join(employeeList, department => department.Id, employee => employee.DepartmentId, (Department, Employee) => new
+            //{
+            //    FullName = Employee.FirstName + "" + Employee.LastName,
+            //    AnnualSalary = Employee.AnnualSalary,
+            //    departmentName = Department.LongName
+            //});
+            //foreach (var item in results)
+            //{
+            //    Console.WriteLine($"{item.FullName,-20}{item.AnnualSalary,10}\t {item.departmentName}");
+            //}
+
+            //Join Operation Example - Query Syntax
+            //var results = from dept in departmentList
+            //              join emp in employeeList
+            //              on dept.Id equals emp.DepartmentId
+            //              select new
+            //              {
+            //                  FullName = emp.FirstName + "" + emp.LastName,
+            //                  AnnualSalary = emp.AnnualSalary,
+            //                  DepartmentName = dept.LongName
+            //              };
+
+            //foreach (var item in results)
+            //{
+            //    Console.WriteLine($"{item.FullName} \t {item.AnnualSalary} \t {item.DepartmentName}"
+            //        );
+            //}
+
+            //Group Join operator  - method syntax
+
+            //var results = departmentList.GroupJoin(employeeList, dept => dept.Id, emp => emp.DepartmentId, (dept, employeesGroup) => new
+            //{
+            //    Employees = employeesGroup,
+            //    DepartmentName = dept.LongName
+            //});
+
+            //foreach (var item in results)
+            //{
+            //    Console.WriteLine($"Department Name : {item.DepartmentName}");
+            //    foreach (var emp in item.Employees)
+            //        Console.WriteLine($"\t{emp.FirstName} {emp.LastName}");
+            //}
+
+            //Group join operator - query syntax
+            var results = from dept in departmentList
+                          join emp in employeeList
+                          on dept.Id equals emp.DepartmentId
+                          into employeeGroup
                           select new
                           {
-                              FullName = emp.FirstName + " " + emp.LastName,
-                              AnnualSalary = emp.AnnualSalary
+                              Employees = employeeGroup,
+                              DepartmentName = dept.LongName
                           };
-            employeeList.Add(new Employee
-            {
-                Id = 5,
-                FirstName = "Prabin",
-                LastName = "Thakur",
-                AnnualSalary = 100000.20m,
-                IsManager = true,
-                DepartmentId = 2
-            }
-                );
+
             foreach (var item in results)
             {
-                Console.WriteLine($"{item.FullName,-20}{item.AnnualSalary,10}");
+                Console.WriteLine($"Department Name: {item.DepartmentName}");
+                foreach (var emp in item.Employees)
+                    Console.WriteLine($"\t {emp.FirstName} {emp.LastName}");
             }
+
             Console.ReadKey();
+        }
+    }
+
+    public static class EnumberableCollectionExtensionMethods
+    {
+        public static IEnumerable<Employee> GetHighSalariedEmployees(this IEnumerable<Employee> employees)
+
+        {
+            foreach (Employee emp in employees)
+            {
+                Console.WriteLine($"Accessing Employee: {emp.FirstName + " " + emp.LastName}");
+                if (emp.AnnualSalary >= 50000)
+                    yield return emp;
+            }
         }
     }
 
@@ -107,9 +220,10 @@
                 LastName = "Joshi",
                 AnnualSalary = 90000.3m,
                 IsManager = false,
-                DepartmentId = 3
+                DepartmentId = 2
             };
             employees.Add(employee);
+
             return employees;
         }
 
